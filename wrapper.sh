d06a20002f15cb1b8f24baa2bada946d654efd00
Cole Brumley <cole.brumley@gmail.com>
2016-04-28 22:36:15 -0500

    make sure to listen on 0.0.0.0 in dev mode

--- wrapper.sh
+++ wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 if [ "$VAULT_DEV" == "1" ] || [ "$VAULT_DEV" == "true" ]; then
-    vault server -dev
+    vault server -dev -dev-listen-address ${VAULT_DEV_LISTEN_ADDRESS:-0.0.0.0:8200}
 else
     confd -backend env -onetime
     vault server -config /etc/vault.hcl -log-level ${VAULT_LOG_LEVEL:-info}
